77af42e6bdd862d54a7cb3402ef8b3b0bd6e8abc
root <root@DESKTOP-3F7B0P6.localdomain>
2020-06-05 00:10:31 -0500

    Render grid within Pick Picture screen

--- App.js
+++ App.js
@@ -7,6 +7,29 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+  const styles = StyleSheet.create({
+    gridView: {
+      marginTop: 10,
+      flex: 1,
+    },
+    itemContainer: {
+      justifyContent: 'flex-end',
+      borderRadius: 5,
+      padding: 10,
+      height: 150,
+    },
+    itemName: {
+      fontSize: 16,
+      color: '#fff',
+      fontWeight: '600',
+    },
+    itemCode: {
+      fontWeight: '600',
+      fontSize: 12,
+      color: '#fff',
+    },
+  });
+
 function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
@@ -32,14 +55,44 @@ function AddText({navigation}) {
 }
 
 function PickPicture({navigation}) {
+  const [items, setItems] = React.useState([
+    { name: 'TURQUOISE', code: '#1abc9c' },
+    { name: 'EMERALD', code: '#2ecc71' },
+    { name: 'PETER RIVER', code: '#3498db' },
+    { name: 'AMETHYST', code: '#9b59b6' },
+    { name: 'WET ASPHALT', code: '#34495e' },
+    { name: 'GREEN SEA', code: '#16a085' },
+    { name: 'NEPHRITIS', code: '#27ae60' },
+    { name: 'BELIZE HOLE', code: '#2980b9' },
+    { name: 'WISTERIA', code: '#8e44ad' },
+    { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
+    { name: 'SUN FLOWER', code: '#f1c40f' },
+    { name: 'CARROT', code: '#e67e22' },
+    { name: 'ALIZARIN', code: '#e74c3c' },
+    { name: 'CLOUDS', code: '#ecf0f1' },
+    { name: 'CONCRETE', code: '#95a5a6' },
+    { name: 'ORANGE', code: '#f39c12' },
+    { name: 'PUMPKIN', code: '#d35400' },
+    { name: 'POMEGRANATE', code: '#c0392b' },
+    { name: 'SILVER', code: '#bdc3c7' },
+    { name: 'ASBESTOS', code: '#7f8c8d' },
+  ]);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>PickPicture</Text>
-      <Button
-        title="forward"
-        onPress={() => navigation.navigate('Pick Music')}
-      />
-    </View>
+    <FlatGrid
+      itemDimension={130}
+      data={items}
+      style={styles.gridView}
+      // staticDimension={300}
+      // fixed
+      spacing={10}
+      renderItem={({ item }) => (
+        <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+          <Text style={styles.itemName}>{item.name}</Text>
+          <Text style={styles.itemCode}>{item.code}</Text>
+        </View>
+      )}
+    />
   );
 }
 
@@ -65,7 +118,7 @@ function FinalResult({navigation}) {
 }
 
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -82,11 +135,5 @@ export default function App() {
 //createStackNativiagtor
 //create grid, putt pictures 100%
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
