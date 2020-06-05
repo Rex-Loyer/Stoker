e09e54f5019962eb2161c799f5aeb997a6ae5bf9
root <root@DESKTOP-3F7B0P6.localdomain>
2020-06-04 19:18:23 -0500

    Sucessfully wrap App.js in Navigation container

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
+  const [outputText, setOutpputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>{outputText}</Text>
+        <Button title="Change Text" onPress={() => setOutpputText('The Text Changed!')}/>
+      </View>
+    </NavigationContainer>
   );
 }
+
+//createStackNativiagtor
+//create grid, putt pictures 100%
 
 const styles = StyleSheet.create({
   container: {
